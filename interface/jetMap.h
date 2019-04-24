13584fa156da0ff411b3ed56d6417954e88b0982
bvermass <basilevermassen@hotmail.com>
2019-04-24 20:57:27 +0200

    Decide on plot boundaries based on minimum eta and phi of constituents. This does not work decently if jet crosses phi discontinuity or lies closely to this, fix it later

--- interface/jetMap.h
+++ interface/jetMap.h
@@ -26,9 +26,17 @@ class jetMap
         jetMap(unsigned constituent_size, double *Pt, double *Eta, double *Phi, double *Mass, int *PdgId, int *Charge, double *dxySig, double *dzSig, int *NHits, int *NPixelHits, bool *HasTrack)
             : nConstituents{constituent_size}, Constituents{new constituent[nConstituents]}
         {
+            Eta_min = 5;
+            Phi_min = 5;
             for(unsigned i = 0; i < nConstituents; i++){
                 Constituents[i] = constituent(*(Pt + i), *(Eta + i), *(Phi + i), *(Mass + i), *(PdgId + i), *(Charge + i), *(dxySig + i), *(dzSig + i), *(NHits + i), *(NPixelHits + i), *(HasTrack + i));
+                Eta_min = std::min(Eta_min, *(Eta + i));
+                Phi_min = std::min(Phi_min, *(Phi + i));
             }
+            Eta_min -= 0.1;
+            Phi_min -= 0.1;
+            Eta_max = Eta_min + 0.5; // always have a 0.5x0.5 grid
+            Phi_max = Phi_min + 0.5;
         }
 };
 
