8ae27c47e567611d3f0eded207c7856e377236c8
bvermass <basilevermassen@hotmail.com>
2019-04-24 18:03:43 +0200

    Make input sampleLists stuff that can read a file containing input root files, output plot directory names and number of plots to be made

--- test/main.cc
+++ test/main.cc
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <TString.h>
 
 #include "../interface/jetMap.h"
 
 #ifndef __CINT__
 int main(int argc, char * argv[])
 {
+    TString inputfile   = argv[1];
+    TString plotdirname = argv[2];
+    int nplots          = atoi(argv[3]);
+    
     std::cout << "Trying to initialize jetMap" << std::endl;
-    jetMap jetmap1(1);
+    jetMap jetmap1(nplots);
     std::cout << "jetmap initialized: " << jetmap1.nConstituents << std::endl;
     return 0;
 }
--- test/run_jetPlotter.sh
+++ test/run_jetPlotter.sh
@@ -7,8 +7,31 @@ if g++ -std=c++0x -o a_jetplotter.out ${headdir}"/test/main.cc" `root-config --c
     echo -e "//COMPILATION SUCCESSFUL//"
     echo -e "//////////////////////////\n"
 
-    ./a_jetplotter.out
+    while IFS='' read -r line || [[ -n "$line" ]]; do
+        if [[ ! "$line" =~ [^[:space:]] ]] || [[ "${line:0:1}" = "#" ]]; then
+            echo -e ""
+        else
+            counter=0
+            for val in $line; do 
+                if [ $counter -eq 0 ]; then
+                    inputfile=$val
+                    counter=1
+                elif [ $counter -eq 1 ]; then
+                    plotdirname=$val
+                    counter=2
+                elif [ $counter -eq 2 ]; then
+                    nplots=$val
+                    counter=3
+                fi
+            done
+             
+            echo "inputfile     :  "$inputfile
+            echo "plotdirname   :  "$plotdirname
+            echo "nplots        :  "$nplots
 
+            ./a_jetplotter.out $inputfile $plotdirname $nplots
+        fi
+    done < "$1"
     rm a_jetplotter.out
 else
     echo -e "\n//////////////////////"
